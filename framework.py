119f60dc042173a9918841a688fab74c39ead4c8
cg592 <ckg35@cornell.edu>
2022-04-10 18:35:40 -0400

    Added framework.py

---
+++ framework.py
@@ -0,0 +1,12 @@
+"""
+Functions we need:
+
+invoke
+query info: remote_qp_num
+            p_key
+            q_key
+            which qp's are taken on a given node
+instantiate a qp (or all 3 on a given node)
+
+No scheduler, probably, just keep it simple
+"""
